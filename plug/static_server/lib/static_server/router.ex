af460d54fb7deba85fd7ded591d099d8460e7d3a
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-10-23 14:59:14 +0900

    update router

--- plug/static_server/lib/static_server/router.ex
+++ plug/static_server/lib/static_server/router.ex
@@ -6,20 +6,26 @@ defmodule StaticServer.Router do
   plug :match
   plug :dispatch
 
+  @root Path.absname("")
+
   get "/" do
-    send_resp conn, 200, Render.top
-  end
-
-  get "/:file_name" do
-    send_resp conn, 200, Render.index(file_name)
-  end
-
-  get "/files/:file_name" do
-    send_resp conn, 200, Render.index(file_name)
+    send_resp conn, 200, Render.index(file_listing("/"), "")
   end
 
   match _ do
-    send400 conn
+    case conn.path_info do
+      ["files"|tail] ->
+        file_path = Path.join tail
+        send_file conn, 200, file_path
+      _ ->
+        file_path = Path.join conn.path_info
+        send_resp conn, 200, Render.index(file_listing(file_path), file_path)
+    end
+  end
+
+  def file_listing(directory) do
+    {:ok, files} = File.ls(@root <> "/" <> directory)
+    files
   end
 
   defp send400(conn) do
@@ -33,23 +39,44 @@ defmodule StaticServer.Render do
   @root Path.absname("")
 
   def top do
-    ~s"""
+    ~S"""
     %html
       %a{:href => '/'}top
     """
     |> Calliope.render
   end
 
-  def index(filename) do
-    ~s"""
+  def index(files, file_path) do
+    ~S"""
     %h1 File Server
     %table
       %tr
         %th File
         %th Size
-
-    file_path: #{@root}/#{filename}
+      - for file <- @files do
+        %tr
+          %td
+            - if @file_path == "" do
+              - case File.dir?(file) do
+                - false ->
+                  %a{title: "#{file}", href: "/files/#{Path.basename(file)}"}= Path.basename(file)
+                - true ->
+                  %a{title: "#{file}", href: "/#{Path.basename(file)}"}= Path.basename(file)
+            - else
+              - case File.dir?(@file_path <> "/" <> file) do
+                - false ->
+                  %a{title: "#{file}", href: "/files/#{@file_path}/#{Path.basename(file)}"}= Path.basename(file)
+                - true ->
+                  %a{title: "#{file}", href: "/#{@file_path}/#{Path.basename(file)}"}= Path.basename(file)
+          %td
+            - if @file_path == "" do
+              - {:ok, %{size: size}} = File.stat(file)
+                = "#{size}b"
+            - else
+              - {:ok, %{size: size}} = File.stat("#{@file_path}/#{Path.basename(file)}")
+                = "#{size}b"
     """
     |> Calliope.render
+    |> EEx.eval_string(assigns: [files: files, file_path: file_path])
   end
 end
