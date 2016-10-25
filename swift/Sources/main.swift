d1ac606d2cbddcab85504dd9b287af63e00007e3
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-10-26 08:08:28 +0900

    add request

--- swift/Sources/main.swift
+++ swift/Sources/main.swift
@@ -5,8 +5,25 @@ HeliumLogger.use()
 
 let router = Router()
 
-router.get("/") { request, response, next in
-  response.send("hello world!")
+router.get("/*") { request, response, next in
+  response.send(
+    "request: \(request)\n" +
+    "request.matchedPath: \(request.matchedPath)\n" +
+    "request.parsedURL.path: \(request.parsedURL.path)\n" +
+    "request.url: \(request.url)\n"
+  )
+  next()
+}
+
+// http://localhost:8090/users/1234 is matched with "/*" and "users/:userId"
+router.get("/users/:userId") { request, response, next in
+  response.send(
+    "request: \(request)\n" +
+    "request.matchedPath: \(request.matchedPath)\n" +
+    "request.parsedURL: \(request.parsedURL)\n" +
+    "request.url: \(request.url)\n" +
+    "request.parameters: \(request.parameters)\n"
+  )
   next()
 }
 
