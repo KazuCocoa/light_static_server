d00c47573462361b34aac034370a4d35b5e6c747
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-10-26 02:21:44 +0900

    add swift server side

---
+++ swift/Sources/main.swift
@@ -0,0 +1,14 @@
+import Kitura
+import HeliumLogger
+
+HeliumLogger.use()
+
+let router = Router()
+
+router.get("/") { request, response, next in
+  response.send("hello world!")
+  next()
+}
+
+Kitura.addHTTPServer(onPort: 8090, with: router)
+Kitura.run()
